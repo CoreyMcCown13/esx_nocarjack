7679853eceeaafe83b6a807a4eac46668f4efa11
Corey McCown <coreymccown13@gmail.com>
2018-02-26 15:00:41 -0500

    Update nocarjack_sv.lua
    Remove unnecessary database calls. Add door lock toggle. Add whitelisted cars. Add owned vehicle support.

--- server/nocarjack_sv.lua
+++ server/nocarjack_sv.lua
@@ -2,56 +2,30 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-local vehicles = {}
+TriggerEvent('es:addGroupCommand', 'lock', 'user', function(source)
+	TriggerClientEvent('esx_nocarjack:toggleLocks', source)
+end, function(source, args, user)
+	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Insufficient Permissions.")
+end, {help = "Toggle the logs in the current car", params = {}})
 
-function getVehData(plate, callback)
-    MySQL.Async.fetchAll("SELECT * FROM `owned_vehicles`", {},
-    function(result)
-        local foundIdentifier = nil
-        for i=1, #result, 1 do
-            local vehicleData = json.decode(result[i].vehicle)
-            if vehicleData.plate == plate then
-                foundIdentifier = result[i].owner
-                break
-            end
-        end
-        if foundIdentifier ~= nil then
-            MySQL.Async.fetchAll("SELECT * FROM `users` WHERE `identifier` = @identifier", {['@identifier'] = foundIdentifier},
-            function(result)
-                local ownerName = result[1].firstname .. " " .. result[1].lastname
-                local info = {
-                    plate = plate,
-                    owner = ownerName
-                }
-                callback(info)
-            end
-          )
-        else -- if identifier is nil then...
-          local info = {
-            plate = plate
-          }
-          callback(info)
-        end
-    end)
-  end
+ESX.RegisterServerCallback('esx_nocarjack:getVehicleOwnerInfo', function(source, cb, plate)
+    MySQL.Async.fetchAll('SELECT * FROM `owned_vehicles` WHERE `vehicle` LIKE @plate', {
+		['@plate'] = "%" .. plate .. "%"
+    },function(result)
+		if result[1] ~= nil then
+			local vehicle      = result[1]
+			local owner   = vehicle['owner']
+			--print ("Owner: " .. owner )
+			cb(owner)
+		else
+			--print("No owner.")
+			cb(false)
+		end
+	end)
+end)
 
 RegisterNetEvent("esx_nocarjack:setVehicleDoorsForEveryone")
-AddEventHandler("esx_nocarjack:setVehicleDoorsForEveryone", function(veh, doors, plate)
-    local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-    local vehicle = veh[1]
-    local veh_doors = veh[2]
-    local veh_plate = veh[3]
-
-    if not vehicles[veh_plate] then
-        getVehData(veh_plate, function(veh_data)
-            if veh_data.owner == nil then
-                local players = GetPlayers()
-                for _,v in pairs(players) do
-                    TriggerClientEvent("esx_nocarjack:setVehicleDoors", v, table.unpack(veh, doors))
-                end
-            end
-        end)
-        vehicles[veh_plate] = true
-    end
+AddEventHandler("esx_nocarjack:setVehicleDoorsForEveryone", function(veh, doors)
+	TriggerClientEvent("esx_nocarjack:setVehicleDoors", -1, veh, doors)
 end)
+
